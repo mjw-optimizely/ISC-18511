44727756b53fb71b649f70013dfb90be47478102
Mike Waldron <91098610+mjw-optimizely@users.noreply.github.com>
2022-09-09 12:18:06 -0600

    Display PaRes and MD rather than validating them

--- routes/index.js
+++ routes/index.js
@@ -7,39 +7,40 @@ router.get("/", function (req, res, next) {
 });
 
 router.post("/validate", function (req, res, next) {
-  var axios = require("axios").default;
+  res.render("display_pares_md", {
+    paRes: req.body.PaRes,
+    md: req.body.MD,
+  });
 
-  var options = {
-    method: "POST",
-    url: "https://uat.gw.fraud.eu.elavonaws.com/3ds2/validate",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization:
-        "Basic ZGQ4ODR0cHlqMzRieXB3YngyYmdqamZiY2hodzpza18yZDkzZ3E5NDlnNzM2YmJ5YjZ3Y2Njanh2M3h3",
-    },
-    data: {
-      PaRes: req.body.PaRes,
-      MD: req.body.MD,
-    },
-  };
+  // var axios = require("axios").default;
 
-  axios
-    .request(options)
-    .then(function (response) {
-      // "authenticationValue":"yZm33IsHWcSd/7BE4U7681Jy2xc=","eci":"05","transStatus":"Y"
-      console.log("auth: " + response.data.rReq.authenticationValue);
-      console.log("trans: " + response.data.rReq.transStatus);
-      console.log("eci: " + response.data.rReq.eci);
-      res.render("validation_success", {
-        response: JSON.stringify(response.data),
-        authenticationValue: response.data.rReq.authenticationValue,
-        transStatus: response.data.rReq.transStatus,
-        eci: response.data.rReq.eci,
-      });
-    })
-    .catch(function (error) {
-      res.render("validation_error", {});
-    });
+  // var options = {
+  //   method: "POST",
+  //   url: "https://uat.gw.fraud.eu.elavonaws.com/3ds2/validate",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //     Authorization:
+  //       "Basic ZGQ4ODR0cHlqMzRieXB3YngyYmdqamZiY2hodzpza18yZDkzZ3E5NDlnNzM2YmJ5YjZ3Y2Njanh2M3h3",
+  //   },
+  //   data: {
+  //     PaRes: req.body.PaRes,
+  //     MD: req.body.MD,
+  //   },
+  // };
+
+  // axios
+  //   .request(options)
+  //   .then(function (response) {
+  //     res.render("validation_success", {
+  //       response: JSON.stringify(response.data),
+  //       authenticationValue: response.data.rReq.authenticationValue,
+  //       transStatus: response.data.rReq.transStatus,
+  //       eci: response.data.rReq.eci,
+  //     });
+  //   })
+  //   .catch(function (error) {
+  //     res.render("validation_error", {});
+  //   });
 });
 
 module.exports = router;
